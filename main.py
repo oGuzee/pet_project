b3170fa3ca143958f19d031361e3abbe26f38d82
Oguzhan Uyar <oguzhanuyar@og.fritz.box>
2019-02-09 18:43:18 +0100

    pylint optimizations

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import datetime
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt  
+import matplotlib.pyplot as plt
 
-from sklearn.cluster import KMeans 
+from sklearn.cluster import KMeans
 from sklearn import metrics
 
 # Closure
-def outerFunction(now = datetime.datetime.now()):
-    def innerFunction(): 
-        print('Application started at: ', now) 
-    innerFunction()
+def outer_function(now=datetime.datetime.now()):
+    def inner_function():
+        print('Application started at: ', now)
+    inner_function()
 
 def load_csv():
     ''' Read lap_times.csv and set the column-headers
@@ -23,10 +23,11 @@ def load_csv():
 def filter_by_race_and_driver_id(df, race, driver):
     ''' Filter lap_times DataFrame by race_id and driver_id
         race_id = int, driver_id = int'''
-    return df[(df.race_id == race) & (lap_times.driver_id == driver)].sort_values(['lap', 'position'])
+    return df[(df.race_id == race) & (df.driver_id == driver)]\
+        .sort_values(['lap', 'position'])
 
 def compute_diff(df):
-    ''' Computing difference of each lap time difference 
+    ''' Computing difference of each lap time difference
         df['diff'] = previous lap + current lap '''
     df = df.set_index(['driver_id', 'lap'])
     df_ = df.groupby(level=0)
@@ -38,31 +39,32 @@ def compute_diff(df):
     return df
 
 def to_numpy_matrix(df):
-    ''' Transform DataFrame to numpy ndarray [lap, lap_time_difference] '''
-    df = df.drop(columns=['lap_milliseconds','position','lap_time', 'race_id', 'driver_id'])  
+    '''Transform DataFrame to numpy ndarray [lap, lap_time_difference]'''
+    df = df.drop(columns=['lap_milliseconds', 'position', 'lap_time', 'race_id', 'driver_id'])
     m = df.values
     print(type(m))
     m = np.delete(m, (0), axis=0)
     return m
 
-def kmeans(m):
-    kmeans = KMeans(n_clusters=5, random_state=41)  
+def k_means(m):
+    ''' KMeans '''
+    kmeans = KMeans(n_clusters=5, random_state=41)
     kmeans.fit(m)
     labels = kmeans.labels_
     print("Cluster centers: ", kmeans.cluster_centers_)
-    print('Labels: ', kmeans.labels_) 
+    print('Labels: ', kmeans.labels_)
 
-    plt.scatter(m[:,0], m[:,1], c= labels, cmap= 'rainbow') 
+    plt.scatter(m[:, 0], m[:, 1], c=labels, cmap='rainbow')
     plt.show()
 
     print('Silhouette score: ', metrics.silhouette_score(m, labels, metric='euclidean'))
     print('Score: ', kmeans.score(m))
 
-if __name__ == '__main__': 
-    outerFunction() # Closure
-    lap_times = load_csv()
-    lap_times = filter_by_race_and_driver_id(lap_times, 841, 20)
-    lap_times = compute_diff(lap_times)
-    lap_times = to_numpy_matrix(lap_times)
-    # print(lap_times)
-    kmeans(lap_times)
+if __name__ == '__main__':
+    outer_function() # Closure
+    LAP_TIMES = load_csv()
+    LAP_TIMES = filter_by_race_and_driver_id(LAP_TIMES, 841, 20)
+    LAP_TIMES = compute_diff(LAP_TIMES)
+    LAP_TIMES = to_numpy_matrix(LAP_TIMES)
+    # print(LAP_TIMES)
+    k_means(LAP_TIMES)
